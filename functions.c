76f547a05b9e787e7d6da370f5afd9cdee8ca9c3
AllanDC1 <adonetticalen@gmail.com>
2024-09-12 22:19:10 -0300

    primeiras funcoes de arquivos criadas, inicio de criacao de usuario

--- functions.c
+++ functions.c
@@ -2,18 +2,90 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
+#include <functions.h>
 
-void criar_usuario(char nome_arquivo[]) {
-    FILE *fP = fopen(nome_arquivo, "ab");
+void limpar_buffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) { }
+}
 
-    if (fP == NULL) {
-        printf("Erro ao tentar abrir arquivo.");
-        return; //add valores para tratamento de erros
+Resposta verificar_arquivo(FILE *ponteiro) {    
+    if (ponteiro == NULL) {    
+        printf("Erro ao acessar arquivo.\n");    
+        return FALHA;
+    }
+    else
+        return OK;
+}
+
+Resposta verificar_cpf(char *entrada_cpf) {
+    if (strlen(entrada_cpf != TAM_CPF - 1)) {
+        printf("CPF possui apenas 11 digitos, insira um CPF valido.\n");
+        return FALHA;
+    }
+    
+    for (int i = 0; i < TAM_CPF - 1; i++) {
+        if (!isdigit(entrada_cpf[i])) {
+            printf("CPF possui apenas numeros, insira um CPF valido.\n");
+            return FALHA; 
+        }
+    }
+    
+    return OK;
+}
+
+// QUANDO FOR CHAMADA ESSA FUNCAO, FAZER O TRATAMENTO DE ERRO (if (ler_usuarios(usuarios, &qnt_usuarios) == FALHA) {return; // voltar pro menu})
+Resposta ler_usuarios(Usuario usuarios[], int *quantidade_lida) {
+    FILE *fP = fopen("dados-usuarios", "rb");
+
+    if (verificar_arquivo(fP) == FALHA) {
+        printf("Erro ao ler dados dos usuarios. Cancelando operacao...\n");       
+        return FALHA;
     }
 
-    // testar quantidade de usuarios ja criados
-    // receber os argumentos necessarios ou pedir
-    // criar/salvar o usuario no arquivo
+    Usuario temp;
+    *quantidade_lida = 0;
+
+    while(fread(&temp, sizeof(Usuario), 1, fP)) {
+        usuarios[*quantidade_lida] = temp; // pega o indice disponivel no array, e armazena o usuario lido nele
+        (*quantidade_lida)++;
+    }
 
     fclose(fP);
+    return OK;
+}
+
+// TALVEZ TROCAR PARA RESPOSTA (TRATAR ERROS MELHOR)
+void criar_usuario() {
+
+    Usuario usuarios[10];
+    int qnt_usuarios = 0;
+    
+    if (ler_usuarios(usuarios, &qnt_usuarios) == FALHA) {
+        return; // voltar pro menu
+    }
+
+    if (qnt_usuarios == MAX_USUARIOS) { // TALVEZ TRATAR ERROS MELHOR
+        printf("Quantidade de usuarios criados atingiu o limite, nao sera possivel criar uma conta.\n");
+        printf("Cancelando operacao...\n");
+        return; // voltar pro menu
+    }
+
+    Usuario novo_usuario;
+    char cpf_temp[13]; // 1 de buffer para conferir tamanho do cpf
+
+    do {
+        printf("Informe seu CPF (apenas numeros): ");
+        fgets(cpf_temp, sizeof(cpf_temp), stdin);
+        printf("\n");
+        limpar_buffer();
+    } while (verificar_cpf(cpf_temp) == FALHA);
+
+    strcpy(novo_usuario.cpf, cpf_temp);
+
+    // PEDIR SENHA, NOME
+    // TERMINAR A CRIACAO DOS DADOS BASICO DO USUARIO    
+    
+    // criar/salvar o usuario no arquivo
 }
--- functions.h
+++ functions.h
@@ -3,11 +3,14 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define TAM_CPF 12 // 11 dígitos + \0 no final
-#define MAX_SENHA 16 // Senha do usuário, no máximo 15 dígitos + \0 no final
-#define TAM_NOME 13 // Primeiro nome, no máximo 12 letras + \0 no final
-#define MAX_TRANSACOES 100
+#define TAM_CPF 12 // 11 dígitos + \0 ou \n no final
+#define MAX_SENHA 16 // Senha do usuário, no máximo 15 dígitos + \0 ou \n no final
+#define TAM_NOME 13 // Primeiro nome, no máximo 12 letras + \0 ou \n no final
 #define TAM_DATA 9 // 00/00/00 + \0 = 9 caracteres
+#define TAM_SIGLA 5 // 4 letras + \0, BTC ou DOGE por exemplo
+
+#define MAX_TRANSACOES 100
+#define MAX_USUARIOS 10 
 
 typedef struct {
     float real;
@@ -32,4 +35,19 @@ typedef struct {
     int qnt_transacoes;
 } Usuario;
 
+typedef struct {
+    char sigla[TAM_SIGLA];
+    float tx_venda;
+    float tx_compra;
+    float cotacao;
+} Moeda;
+
 typedef enum {DEPOSITO = 1, SAQUE = 2, COMPRA = 3, VENDA = 4} ETipoTransacao;
+typedef enum {OK, FALHA} Resposta;
+
+//Declaração de funções
+
+void limpar_buffer(void);
+Resposta verificar_arquivo(FILE *ponteiro);
+Resposta verificar_cpf(char *entrada_cpf);
+Resposta ler_usuarios(Usuario usuarios[], int *quantidade_lida);
