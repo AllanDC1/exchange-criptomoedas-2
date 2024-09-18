40966b4f87f36de1e9660d8a025289d630f3de56
AllanDC1 <162832747+AllanDC1@users.noreply.github.com>
2024-09-18 19:55:16 +0000

    base do primeiro menu

--- main.c
+++ main.c
@@ -2,9 +2,34 @@
 #include "functions.h"
 
 int main(){
+    int op;
 
     printf("\nBem-Vindo ao Exchange de CriptoMoedas!\n");
-    printf("\n");    
+    printf("\n 1. Login\n");
+    printf(" 2. Registrar-se\n");
+    printf(" 3. Excluir conta\n");
+
+    do {
+        printf("\nEscolha a operação:");
+        scanf("%d", &op);
+        printf("\n");
+
+        if (op != 1 && op != 2 && op != 3) {
+            printf("Operação inválida, insira novamente.\n");
+        }
+    }while (op != 1 && op != 2 && op != 3);
+
+    switch op {
+        case 1:
+            // login();
+            break;
+        case 2:
+            criar_usuario();
+            break;
+        case 3:
+            // excluir_usuario();
+            break;
+    }
 
     return 0;
 }
